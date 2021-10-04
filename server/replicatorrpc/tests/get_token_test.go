e005a1bb58cd9d122aace1f61171feda6d71c0d5
IntegralTeam <repo@sfxdx.ru>
2021-10-04 17:51:31 +0300

    added syncchain and get token tests

--- server/replicatorrpc/tests/get_token_test.go
+++ server/replicatorrpc/tests/get_token_test.go
@@ -1,5 +1,46 @@
 package replicatorrpc
 
-func (suite *TestSuite) TestGetToken() {
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"token-strike/tsp2p/server/replicator"
+)
 
+func (suite *TestSuite) TestGetToken() {
+	type args struct {
+		ctx context.Context
+		req *replicator.GetTokenRequest
+	}
+	tests := []struct {
+		name       string
+		args       args
+		want       *replicator.GetTokenResponse
+		wantErr    bool
+		wantErrMsg string
+	}{
+		{
+			name: "valid",
+			args: args{
+				ctx: context.Background(),
+				req: &replicator.GetTokenRequest{
+					TokenId: "t1",
+				},
+			},
+			want:       nil,
+			wantErr:    false,
+			wantErrMsg: "",
+		},
+	}
+
+	for _, tt := range tests {
+		suite.T().Run(tt.name, func(t *testing.T) {
+			got, err := suite.grpcClient.GetToken(tt.args.ctx, tt.args.req)
+			if tt.wantErr {
+				require.EqualError(t, err, tt.wantErrMsg)
+				return
+			}
+			require.Equal(t, tt.want, got)
+		})
+	}
 }
--- server/replicatorrpc/tests/sync_chain_test.go
+++ server/replicatorrpc/tests/sync_chain_test.go
@@ -1,3 +1,48 @@
 package replicatorrpc
 
-func (suite *TestSuite) TestSyncChain() {}
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"token-strike/tsp2p/server/replicator"
+)
+
+func (suite *TestSuite) TestSyncChain() {
+	type args struct {
+		ctx context.Context
+		req *replicator.SyncChainRequest
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantErr    bool
+		wantErrMsg string
+	}{
+		{
+			name: "valid",
+			args: args{
+				ctx: context.Background(),
+				req: &replicator.SyncChainRequest{
+					Name:   "t1",
+					Blocks: nil,
+				},
+			},
+			wantErr:    false,
+			wantErrMsg: "",
+		},
+	}
+
+	for _, tt := range tests {
+		suite.T().Run(tt.name, func(t *testing.T) {
+			c, err := suite.grpcClient.SyncChain(tt.args.ctx)
+			require.NoError(t, err)
+			err = c.Send(tt.args.req)
+			if tt.wantErr {
+				require.EqualError(t, err, tt.wantErrMsg)
+				return
+			}
+			require.NoError(t, err)
+		})
+	}
+
+}
