435c403d80ce3aea92000ea2c772e1ee293343c5
IntegralTeam <repo@sfxdx.com>
2021-10-05 16:28:09 +0200

    feat: added logic for GetHeaders method

--- server/replicatorrpc/get_headers.go
+++ server/replicatorrpc/get_headers.go
@@ -3,12 +3,18 @@ package replicatorrpc
 import (
 	"context"
 
+	"token-strike/tsp2p/server/DB"
 	"token-strike/tsp2p/server/replicator"
-
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func (s *Server) GetHeaders(ctx context.Context, req *replicator.GetHeadersRequest) (*replicator.GetHeadersResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "GetHeaders not implimented")
+	blocks, err := s.db.GetMerkleBlockDB(req.TokenId, req.Hash)
+	if err != nil {
+		return nil, err
+	}
+
+	return &replicator.GetHeadersResponse{
+		Token:  &DB.Token{},
+		Blocks: blocks,
+	}, nil
 }
