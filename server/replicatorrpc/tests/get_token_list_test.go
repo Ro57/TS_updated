03286f2bab95ebbb743e279bd35631cf31e079f6
IntegralTeam <repo@sfxdx.com>
2021-10-05 10:03:43 +0300

    added get token list and fix get token test

--- server/replicatorrpc/tests/get_token_list_test.go
+++ server/replicatorrpc/tests/get_token_list_test.go
@@ -1,4 +1,72 @@
 package replicatorrpc
 
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"token-strike/tsp2p/server/DB"
+	"token-strike/tsp2p/server/replicator"
+)
+
 func (suite *TestSuite) TestGetTokenList() {
+	type args struct {
+		ctx context.Context
+		req *replicator.GetTokenListRequest
+	}
+	tests := []struct {
+		name       string
+		args       args
+		want       *replicator.GetTokenListResponse
+		wantErr    bool
+		wantErrMsg string
+	}{
+		{
+			name: "valid",
+			args: args{
+				ctx: context.Background(),
+				req: &replicator.GetTokenListRequest{
+					IssuerId: "issuerid",
+					Params:   nil,
+				},
+			},
+			want: &replicator.GetTokenListResponse{
+				Tokens: []*replicator.Token{
+					{
+						Name: "t1",
+						Token: &DB.Token{
+							Count:        5,
+							Expiration:   99999999,
+							Creation:     999,
+							IssuerPubkey: "issuerid",
+						},
+						Root: "some root 1",
+					},
+					{
+						Name: "t2",
+						Token: &DB.Token{
+							Count:        5,
+							Expiration:   99999999,
+							Creation:     9999,
+							IssuerPubkey: "issuerid",
+						},
+						Root: "some root 2",
+					},
+				},
+				Total: 10,
+			},
+			wantErr:    false,
+			wantErrMsg: "",
+		},
+	}
+
+	for _, tt := range tests {
+		suite.T().Run(tt.name, func(t *testing.T) {
+			got, err := suite.grpcClient.GetTokenList(tt.args.ctx, tt.args.req)
+			if tt.wantErr {
+				require.EqualError(t, err, tt.wantErrMsg)
+				return
+			}
+			require.Equal(t, tt.want, got)
+		})
+	}
 }
