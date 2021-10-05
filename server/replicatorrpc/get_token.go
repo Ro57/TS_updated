b46fbacc450e8518ba463edd5432e953499d5283
IntegralTeam <repo@sfxdx.com>
2021-10-05 16:26:14 +0200

    feat: added logic for GetToken method

--- server/replicatorrpc/get_token.go
+++ server/replicatorrpc/get_token.go
@@ -4,11 +4,15 @@ import (
 	"context"
 
 	"token-strike/tsp2p/server/replicator"
-
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func (s *Server) GetToken(ctx context.Context, req *replicator.GetTokenRequest) (*replicator.GetTokenResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "GetToken not implimented")
+	token, err := s.db.GetToken(req.TokenId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &replicator.GetTokenResponse{
+		Token: &token,
+	}, nil
 }
