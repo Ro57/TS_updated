793993634db67c0a22d245cbbf4d49fa71e7a95e
IntegralTeam <repo@sfxdx.com>
2021-10-06 13:17:32 +0300

    test: introduce testify on tokendb

--- internal/database/tokendb_test.go
+++ internal/database/tokendb_test.go
@@ -9,6 +9,8 @@ import (
 	"token-strike/tsp2p/server/DB"
 	"token-strike/tsp2p/server/justifications"
 
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 	"go.etcd.io/bbolt"
 )
 
@@ -17,117 +19,146 @@ const (
 	name = "./test.db"
 )
 
-func TestCreateDB(t *testing.T) {
-	ts, err := Connect(name)
+const (
+	TypeUpdate = "update"
+	TypeView   = "view"
+)
+
+func TestExampleTestSuite(t *testing.T) {
+	suite.Run(t, new(TestSuite))
+}
+
+type TestSuite struct {
+	suite.Suite
+	db *TokenStrikeDB
+}
+
+func (suite *TestSuite) SetupTest() {
+	db, err := Connect(name)
 	if err != nil {
-		t.Fatalf("Connection refused %v", err)
+		suite.T().Fatalf("Connection refused %v", err)
 	}
 
-	defer ts.Close()
+	suite.db = db
+}
 
-	err = ts.Ping()
+func (suite *TestSuite) AfterTest(suiteName, testName string) {
+	err := suite.db.Clear()
 	if err != nil {
-		t.Fatal("db not connected: ", err)
+		suite.T().Fatalf("Clear test file exception %v", err)
+	}
+	suite.db.Close()
+}
+
+func (suite *TestSuite) selectType(txType string) func(func(*bbolt.Tx) error) error {
+	switch txType {
+	case TypeUpdate:
+		return suite.db.Update
+	case TypeView:
+		return suite.db.View
+	}
+	return nil
+}
+
+func (suite *TestSuite) TestConectDB() {
+	suite.T().Run("create DB", func(t *testing.T) {
+		err := suite.db.Ping()
+		require.NoError(t, err, "db not connected: ")
+	})
+
+	suite.T().Run("close DB ", func(t *testing.T) {
+		suite.db.Close()
+		err := suite.db.Ping()
+		require.Error(t, err, "ping after close connection")
+	})
+}
+
+func (suite *TestSuite) TestEmployee() {
+	testTransactions := []struct {
+		name   string
+		txType string
+		tx     func(*bbolt.Tx) error
+	}{
+		{
+			name:   "create business",
+			txType: TypeUpdate,
+			tx: func(tx *bbolt.Tx) error {
+				_, err := tx.CreateBucket([]byte("Business"))
+
+				if err != nil {
+					return fmt.Errorf("create bucket: %s", err)
+				}
+
+				return nil
+			},
+		},
+		{
+			name:   "create employee bucket",
+			txType: TypeUpdate,
+			tx: func(tx *bbolt.Tx) error {
+				b := tx.Bucket([]byte("Business"))
+
+				_, err := b.CreateBucket([]byte("Employee"))
+				if err != nil {
+					return fmt.Errorf("create bucket: %s", err)
+				}
+
+				return nil
+
+			},
+		},
+		{
+			name:   "create emp1",
+			txType: TypeUpdate,
+			tx: func(tx *bbolt.Tx) error {
+				b := tx.Bucket([]byte("Business"))
+				emp := b.Bucket([]byte("Employee"))
+
+				err := b.Put([]byte("Emp1"), []byte("Number 100"))
+
+				if err != nil {
+					return err
+				}
+
+				err = emp.Put([]byte("Time"), []byte("180h"))
+
+				return err
+
+			},
+		},
+		{
+			name:   "get emp1 from db",
+			txType: TypeView,
+			tx: func(tx *bbolt.Tx) error {
+				b := tx.Bucket([]byte("Business"))
+				emp := b.Bucket([]byte("Employee"))
+
+				empNum := b.Get([]byte("Emp1"))
+				if string(empNum) != "Number 100" {
+					return fmt.Errorf("want string 'Number 100' but get '%s'", string(empNum))
+				}
+
+				empTime := emp.Get([]byte("Time"))
+				if string(empTime) != "180h" {
+					return fmt.Errorf("want string '180h' but get '%s'", string(empTime))
+				}
+
+				return nil
+			},
+		},
+	}
+
+	for _, tt := range testTransactions {
+		suite.T().Run(tt.name, func(t *testing.T) {
+			err := suite.selectType(tt.txType)(tt.tx)
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
 	}
 }
 
-func TestPing(t *testing.T) {
-	ts, err := Connect(name)
-	if err != nil {
-		t.Fatalf("Connection refused %v", err)
-	}
-
-	ts.Close()
-	err = ts.Ping()
-
-	if err == nil {
-		t.Fatal("ping after close connection")
-	}
-}
-
-func TestEmployeeUpdateView(t *testing.T) {
-	ts, err := Connect(name)
-	if err != nil {
-		t.Fatalf("Connection refused %v", err)
-	}
-
-	defer ts.Close()
-
-	defer func() {
-		err = ts.Clear()
-		if err != nil {
-			t.Fatal("Clear failed", err)
-		}
-	}()
-
-	err = ts.Update(func(tx *bbolt.Tx) error {
-		_, err := tx.CreateBucket([]byte("Business"))
-
-		if err != nil {
-			return fmt.Errorf("create bucket: %s", err)
-		}
-
-		return nil
-	})
-	if err != nil {
-		t.Fatal("Create top level busket structures failed: ", err)
-	}
-
-	err = ts.Update(func(tx *bbolt.Tx) error {
-		b := tx.Bucket([]byte("Business"))
-
-		_, err = b.CreateBucket([]byte("Employee"))
-		if err != nil {
-			return fmt.Errorf("create bucket: %s", err)
-		}
-
-		return nil
-
-	})
-	if err != nil {
-		t.Fatal("Create busket structures failed: ", err)
-	}
-
-	err = ts.Update(func(tx *bbolt.Tx) error {
-		b := tx.Bucket([]byte("Business"))
-		emp := b.Bucket([]byte("Employee"))
-
-		err := b.Put([]byte("Emp1"), []byte("Number 100"))
-
-		if err != nil {
-			return err
-		}
-
-		err = emp.Put([]byte("Time"), []byte("180h"))
-
-		return err
-	})
-	if err != nil {
-		t.Fatal("Put information into bucket: ", err)
-	}
-
-	err = ts.View(func(tx *bbolt.Tx) error {
-		b := tx.Bucket([]byte("Business"))
-		emp := b.Bucket([]byte("Employee"))
-
-		empNum := b.Get([]byte("Emp1"))
-		if string(empNum) != "Number 100" {
-			t.Fatalf("want string 'Number 100' but get '%s'", string(empNum))
-		}
-
-		empTime := emp.Get([]byte("Time"))
-		if string(empTime) != "180h" {
-			t.Fatalf("want string '180h' but get '%s'", string(empTime))
-		}
-
-		return nil
-	})
-	if err != nil {
-		t.Fatal("Get information from bucket: ", err)
-	}
-}
-
-func TestTokenBlock(t *testing.T) {
+func (suite *TestSuite) TestTokenBlock() {
 	var wantExpBlockNumber int32 = 2000
 	wantCreateTime := time.Now()
 	tokenName := "smt"
@@ -165,99 +196,100 @@ func TestTokenBlock(t *testing.T) {
 
 	var lastBlock [sha256.Size]byte
 
-	ts, err := Connect(name)
-	if err != nil {
-		t.Fatalf("Connection refused %v", err)
+	testTransactions := []struct {
+		name   string
+		txType string
+		tx     func(*bbolt.Tx) error
+	}{
+		{
+			name:   "create blockchain",
+			txType: TypeUpdate,
+			tx: func(tx *bbolt.Tx) error {
+				b, err := tx.CreateBucket([]byte(tokenName))
+				if err != nil {
+					return fmt.Errorf("create top level bucket: %s", err)
+				}
+
+				chain, err := b.CreateBucket([]byte("chain"))
+				if err != nil {
+					return fmt.Errorf("create chain bucket: %s", err)
+				}
+				tokenByte, nativeErr := json.Marshal(wantToken)
+				if nativeErr != nil {
+					return fmt.Errorf("(update) marshal token structure: %s", nativeErr)
+				}
+
+				err = b.Put([]byte("Info"), tokenByte)
+				if err != nil {
+					return fmt.Errorf("put token %s", err)
+				}
+
+				blockByte, nativeErr := json.Marshal(wantBlock)
+				if nativeErr != nil {
+					return fmt.Errorf("(update) marshal block structure: %s", nativeErr)
+				}
+
+				lastBlock = sha256.Sum256(blockByte)
+
+				err = chain.Put(lastBlock[:], blockByte)
+				if err != nil {
+					return fmt.Errorf("put block %s", err)
+				}
+
+				return nil
+			},
+		},
+		{
+			name:   "get data from blockchain",
+			txType: TypeView,
+			tx: func(tx *bbolt.Tx) error {
+				b := tx.Bucket([]byte(tokenName))
+				if b == nil {
+					return fmt.Errorf("bucket %s not found", tokenName)
+				}
+
+				chain := b.Bucket([]byte("chain"))
+				if chain == nil {
+					return fmt.Errorf("bucket chain not found")
+				}
+
+				marshalToken, nativeErr := json.Marshal(wantToken)
+				if nativeErr != nil {
+					return fmt.Errorf("(view) marshal token structure: %s", nativeErr)
+				}
+
+				tokenByte := b.Get([]byte("Info"))
+				if tokenByte == nil {
+					return fmt.Errorf("token info with name %s not found", tokenByte)
+				}
+				if string(marshalToken) != string(tokenByte) {
+					return fmt.Errorf("want token structure %s but get %s", marshalToken, tokenByte)
+				}
+
+				marshalBlock, nativeErr := json.Marshal(wantBlock)
+				if nativeErr != nil {
+					return fmt.Errorf("(view) marshal token structure: %s", nativeErr)
+				}
+
+				blockByte := chain.Get(lastBlock[:])
+				if blockByte == nil {
+					return fmt.Errorf("block with hash %s not found", string(lastBlock[:]))
+				}
+				if string(marshalBlock) != string(blockByte) {
+					return fmt.Errorf("want block structure %s but get %s", marshalBlock, blockByte)
+				}
+
+				return nil
+			},
+		},
 	}
 
-	defer ts.Close()
-
-	defer func() {
-		err := ts.Clear()
-		if err != nil {
-			t.Fatal("clear failed", err)
-		}
-	}()
-
-	err = ts.Update(func(tx *bbolt.Tx) error {
-		b, err := tx.CreateBucket([]byte(tokenName))
-		if err != nil {
-			t.Fatalf("create top level bucket: %s", err)
-		}
-
-		chain, err := b.CreateBucket([]byte("chain"))
-		if err != nil {
-			t.Fatalf("create chain bucket: %s", err)
-		}
-		tokenByte, nativeErr := json.Marshal(wantToken)
-		if nativeErr != nil {
-			t.Fatalf("(update) marshal token structure: %s", nativeErr)
-		}
-
-		err = b.Put([]byte("Info"), tokenByte)
-		if err != nil {
-			t.Fatalf("put token %s", err)
-		}
-
-		blockByte, nativeErr := json.Marshal(wantBlock)
-		if nativeErr != nil {
-			t.Fatalf("(update) marshal block structure: %s", nativeErr)
-		}
-
-		lastBlock = sha256.Sum256(blockByte)
-
-		err = chain.Put(lastBlock[:], blockByte)
-		if err != nil {
-			t.Fatalf("put block %s", err)
-		}
-
-		return nil
-	})
-	if err != nil {
-		t.Fatal("generate DB structure: ", err)
+	for _, tt := range testTransactions {
+		suite.T().Run(tt.name, func(t *testing.T) {
+			err := suite.selectType(tt.txType)(tt.tx)
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
 	}
-
-	err = ts.View(func(tx *bbolt.Tx) error {
-		b := tx.Bucket([]byte(tokenName))
-		if b == nil {
-			t.Fatalf("bucket %s not found", tokenName)
-		}
-
-		chain := b.Bucket([]byte("chain"))
-		if chain == nil {
-			t.Fatal("bucket chain not found")
-		}
-
-		marshalToken, nativeErr := json.Marshal(wantToken)
-		if nativeErr != nil {
-			t.Fatalf("(view) marshal token structure: %s", nativeErr)
-		}
-
-		tokenByte := b.Get([]byte("Info"))
-		if tokenByte == nil {
-			t.Fatalf("token info with name %s not found", tokenByte)
-		}
-		if string(marshalToken) != string(tokenByte) {
-			t.Fatalf("want token structure %s but get %s", marshalToken, tokenByte)
-		}
-
-		marshalBlock, nativeErr := json.Marshal(wantBlock)
-		if nativeErr != nil {
-			t.Fatalf("(view) marshal token structure: %s", nativeErr)
-		}
-
-		blockByte := chain.Get(lastBlock[:])
-		if blockByte == nil {
-			t.Fatalf("block with hash %s not found", string(lastBlock[:]))
-		}
-		if string(marshalBlock) != string(blockByte) {
-			t.Fatalf("want block structure %s but get %s", marshalBlock, blockByte)
-		}
-
-		return nil
-	})
-	if err != nil {
-		t.Fatal("read data from DB: ", err)
-	}
-
 }
