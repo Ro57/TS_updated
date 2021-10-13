b3123ee0b5f19ae763842e8818ff4456a13a49c3
IntegralTeam <repo@sfxdx.com>
2021-10-13 15:33:11 +0300

    fix(sprint1): remove trash

--- internal/utils/address/generate_key.go
+++ internal/utils/address/generate_key.go
@@ -1,14 +1,5 @@
 package utils
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-import "token-strike/internal/types"
-
-func (e *Address) GenerateKey(randomSeed []byte) types.Key {
-	return types.Key{}
-=======
-=======
->>>>>>> 1819400 (feat(sprint1): add GenerateKey method)
 import (
 	ed "crypto/ed25519"
 
@@ -17,8 +8,4 @@ import (
 
 func (e *Address) GenerateKey(randomSeed []byte) types.Key {
 	return ed.NewKeyFromSeed(randomSeed)
-<<<<<<< HEAD
->>>>>>> 1819400 (feat(sprint1): add GenerateKey method)
-=======
->>>>>>> 1819400 (feat(sprint1): add GenerateKey method)
 }
