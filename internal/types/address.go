a9586293f3bc761687ba721a689852b54ead1530
IntegralTeam <repo@sfxdx.com>
2021-10-13 15:30:01 +0300

    fix(sprint1): remove merge helper

--- internal/types/address.go
+++ internal/types/address.go
@@ -4,11 +4,7 @@ import (
 	ed "crypto/ed25519"
 )
 
-<<<<<<< HEAD:internal/types/address.go
-type Key ed.PrivateKey
-=======
 type Key = ed.PrivateKey
->>>>>>> 7fa4619 (feat(sprint1): add utils package):internal/utils/address.go
 
 type Address interface {
 	CheckSig(address string, signature []byte, data []byte) bool
