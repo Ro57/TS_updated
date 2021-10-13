93c013607c5d08154a7eeb5a0867bebcea94bd62
IntegralTeam <repo@sfxdx.com>
2021-10-13 15:19:02 +0300

    feat(sprint1): add GenerateKey method

--- internal/utils/address/generate_key.go
+++ internal/utils/address/generate_key.go
@@ -1,11 +1,14 @@
 package utils
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import "token-strike/internal/types"
 
 func (e *Address) GenerateKey(randomSeed []byte) types.Key {
 	return types.Key{}
 =======
+=======
+>>>>>>> 1819400 (feat(sprint1): add GenerateKey method)
 import (
 	ed "crypto/ed25519"
 
@@ -14,5 +17,8 @@ import (
 
 func (e *Address) GenerateKey(randomSeed []byte) types.Key {
 	return ed.NewKeyFromSeed(randomSeed)
+<<<<<<< HEAD
+>>>>>>> 1819400 (feat(sprint1): add GenerateKey method)
+=======
 >>>>>>> 1819400 (feat(sprint1): add GenerateKey method)
 }
