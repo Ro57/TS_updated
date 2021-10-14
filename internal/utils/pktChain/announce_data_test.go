8294088051f1d602e0164999db3603b02be6d2f4
IntegralTeam <repo@sfxdx.com>
2021-10-14 17:01:27 +0300

    test(sprint1): add announce data tests

---
+++ internal/utils/pktChain/announce_data_test.go
@@ -0,0 +1,39 @@
+package utils
+
+func (suite *TestSuite) TestAnnounceData() {
+
+	type args struct {
+		payload []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    func(expected interface{}, actual interface{}, msgAndArgs ...interface{}) bool
+		wantNum int32
+	}{
+		{
+			name: "Valid annoncment num",
+			args: args{
+				payload: []byte{},
+			},
+			want:    suite.Equal,
+			wantNum: 0,
+		},
+		{
+			name: "Invalid annoncment num",
+			args: args{
+				payload: []byte{},
+			},
+			want:    suite.NotEqual,
+			wantNum: 1,
+		},
+	}
+
+	for _, tt := range tests {
+		suite.Run(tt.name, func() {
+			annChan := suite.chain.AnnounceData(tt.args.payload)
+			tempAnn := <-annChan
+			tt.want(tt.wantNum, tempAnn.Num, "error in %v want %v but got %v", tt.name, tt.wantNum, tempAnn.Num)
+		})
+	}
+}
