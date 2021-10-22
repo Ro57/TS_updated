af79783b4a606aff2a5cac02952399165f493512
IntegralTeam <repo@sfxdx.com>
2021-10-22 15:42:35 +0300

    test(sprint2(: init tokenstrikemock test

---
+++ internal/utils/tests/tokenstrikemock/tokenstrikemock_test.go
@@ -0,0 +1,55 @@
+package utils_test
+
+import (
+	"testing"
+	"token-strike/internal/database"
+	"token-strike/internal/database/repository"
+	"token-strike/internal/utils"
+	"token-strike/internal/utils/tokenstrikemock"
+
+	"github.com/stretchr/testify/suite"
+)
+
+func TestExampleTestSuite(t *testing.T) {
+	suite.Run(t, new(TestSuite))
+}
+
+type TestSuite struct {
+	suite.Suite
+	tokenStrike tokenstrikemock.TokenStrikeMock
+}
+
+func randomSeed(l, offset int) [32]byte {
+	bytes := [32]byte{}
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(i + offset)
+	}
+	return bytes
+}
+
+func (suite *TestSuite) SetupTest() {
+	db, err := database.Connect("./test.db")
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			suite.Error(err)
+		}
+
+		err = db.Clear()
+		if err != nil {
+			suite.Error(err)
+		}
+
+	}()
+
+	tokendb := repository.NewBbolt(db)
+
+	activeAddressScheme := &utils.SimpleAddressScheme{}
+	privKey := activeAddressScheme.GenerateKey(randomSeed(32, 0))
+
+	suite.tokenStrike = *tokenstrikemock.New(tokendb, privKey.Address())
+}
