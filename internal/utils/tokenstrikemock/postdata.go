a9f02f3147e6737044ed589b88f44d7787d7484b
IntegralTeam <repo@sfxdx.com>
2021-10-25 18:03:01 +0300

    fix(sprint3): rework apply justification

--- internal/utils/tokenstrikemock/postdata.go
+++ internal/utils/tokenstrikemock/postdata.go
@@ -55,6 +55,12 @@ func (t TokenStrikeMock) validateBlock(block *DB.Block) (warnings []string, err 
 	return nil, nil
 }
 
+//TODO: place here checking for ret error with warnings
+func validateTransfer(block *DB.Block) (warnings []string, err error) {
+
+	return nil, nil
+}
+
 //TODO: place here checking for ret warnings
 func (t TokenStrikeMock) validateLock(reqLock lock.Lock) (warnings []string, err error) {
 	validatorErrors := []error{
