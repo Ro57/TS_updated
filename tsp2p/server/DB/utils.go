4c76a004217b287926d26ec5a4661baaf1481eb7
IntegralTeam <repo@sfxdx.com>
2021-10-25 13:40:53 +0200

    fix: test for method Equal for the simple private key

--- tsp2p/server/DB/utils.go
+++ tsp2p/server/DB/utils.go
@@ -2,8 +2,22 @@ package DB
 
 import (
 	"crypto/sha256"
+	"encoding/hex"
 	"github.com/golang/protobuf/proto"
+	"token-strike/internal/types"
 )
+
+// --------------- Block -------------------
+
+// Sing signed block received private key and stored signature
+func (m *Block) Sing(key types.PrivateKey) error {
+	bytes, err := proto.Marshal(m)
+	if err != nil {
+		return err
+	}
+	m.Signature = hex.EncodeToString(key.Sign(bytes))
+	return nil
+}
 
 func (m Block) GetHash() ([]byte, error) {
 	data, err := proto.Marshal(&m)
@@ -13,6 +27,8 @@ func (m Block) GetHash() ([]byte, error) {
 	res := sha256.Sum256(data)
 	return res[:], nil
 }
+
+// --------------- State -------------------
 
 func (m State) GetHash() ([]byte, error) {
 	stateBytes, err := proto.Marshal(&m)
--- tsp2p/server/lock/utils.go
+++ tsp2p/server/lock/utils.go
@@ -2,8 +2,19 @@ package lock
 
 import (
 	"crypto/sha256"
+	"encoding/hex"
 	"github.com/golang/protobuf/proto"
+	"token-strike/internal/types"
 )
+
+func (m *Lock) Sing(key types.PrivateKey) error {
+	bytes, err := proto.Marshal(m)
+	if err != nil {
+		return err
+	}
+	m.Signature = hex.EncodeToString(key.Sign(bytes))
+	return nil
+}
 
 func (m Lock) GetHash() ([]byte, error) {
 	bytes, err := proto.Marshal(&m)
