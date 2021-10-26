e9fce278782425783d7f054101e8f7373ddec61d
IntegralTeam <repo@sfxdx.com>
2021-10-26 10:26:08 +0200

    feat: added methods for database repository

--- internal/database/repository.go
+++ internal/database/repository.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"token-strike/tsp2p/server/DB"
+	"token-strike/tsp2p/server/lock"
 	"token-strike/tsp2p/server/replicator"
 )
 
@@ -13,14 +14,13 @@ type DBRepository interface {
 type SaveRepository interface {
 	SaveBlock(name string, block *DB.Block) error
 	SyncBlock(name string, blocks []*DB.Block) error
-
 	// Add new token to local DB
 	SaveIssuerTokenDB(name string, issuer string) error
-
 	AssemblyBlock(name string, justifications []*DB.Justification) (*DB.Block, error)
-
 	// Issue new token
 	IssueTokenDB(name string, offer *DB.Token, block *DB.Block, state *DB.State) error
+	TransferTokens(htlc, lock string) error
+	LockToken(lock *lock.Lock) error
 }
 
 type GetRepository interface {
