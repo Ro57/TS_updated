ecf98f2097e5242e5069b79f8849328d666022a1
IntegralTeam <repo@sfxdx.com>
2021-10-26 09:06:55 +0200

    ref: added errors

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -5,11 +5,13 @@ import "errors"
 var (
 	EmptyAddressErr = errors.New("empty address")
 
-	TokensDBNotFound       = errors.New("tokens DB not created")
+	LockNotFoundErr        = errors.New("lock not found")
+	OwnerNoFoundErr        = errors.New("owner not found")
 	InfoNotFoundErr        = errors.New("token info not found")
 	StateNotFoundErr       = errors.New("state not found")
 	TokenNotFoundErr       = errors.New("token does not found")
 	BlockNotFoundErr       = errors.New("block info not found")
+	TokensDBNotFound       = errors.New("tokens DB not created")
 	RootHashNotFoundErr    = errors.New("root hash not found")
 	LastBlockNotFoundErr   = errors.New("last block not found")
 	ChainBucketNotFoundErr = errors.New("chain bucket not found")
