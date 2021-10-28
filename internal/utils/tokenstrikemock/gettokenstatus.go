552d0841461e824f23453d34ea293bf823b6471f
IntegralTeam <repo@sfxdx.com>
2021-10-28 19:06:12 +0300

    feat(sprint3): add GetTokenStatus call method

--- internal/utils/tokenstrikemock/gettokenstatus.go
+++ internal/utils/tokenstrikemock/gettokenstatus.go
@@ -4,13 +4,12 @@ import (
 	"context"
 	"errors"
 	defaultError "token-strike/internal/errors"
-	"token-strike/tsp2p/server/DB"
 	"token-strike/tsp2p/server/tokenstrike"
 )
 
 func (t *TokenStrikeMock) GetTokenStatus(ctx context.Context, req *tokenstrike.TokenStatusReq) (*tokenstrike.TokenStatus, error) {
 
-	token, err := t.bboltDB.GetToken(req.Tokenid)
+	_, err := t.bboltDB.GetToken(req.Tokenid)
 
 	if errors.Is(defaultError.TokenNotFoundErr, err) {
 		return nil, nil
@@ -20,13 +19,18 @@ func (t *TokenStrikeMock) GetTokenStatus(ctx context.Context, req *tokenstrike.T
 		return nil, err
 	}
 
+	block0, state0, err := t.bboltDB.GetTokenStatus(req.Tokenid)
+	if err != nil {
+		return nil, err
+	}
+
 	resp := &tokenstrike.TokenStatus{
 		CurrentHeight: uint32(t.pktChain.CurrentHeight()),
 		CurrentHash:   req.Tokenid,
 
 		// TODO: change to real one
-		Dblock0: &DB.Block{},
-		State0:  &DB.State{Token: token.Token},
+		Dblock0: block0,
+		State0:  state0,
 	}
 
 	return resp, nil
