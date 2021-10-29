c5d99f716c1672f27e634989d9f385968d84d8d2
IntegralTeam <repo@sfxdx.com>
2021-10-29 09:32:06 +0200

    fix: the wallet interface

--- internal/utils/wallet/sendtokens.go
+++ internal/utils/wallet/sendtokens.go
@@ -2,17 +2,12 @@ package wallet
 
 import (
 	"crypto/sha256"
-	"encoding/hex"
 	"token-strike/tsp2p/server/tokenstrike"
 
 	"github.com/golang/protobuf/proto"
 )
 
-func (s SimpleWallet) SendTokens(tokenId string, lockId []byte, secret string) ([]byte, error) {
-	secretHex, err := hex.DecodeString(secret)
-	if err != nil {
-		return nil, err
-	}
+func (s SimpleWallet) SendTokens(tokenId string, lockId []byte, secretHex []byte) ([]byte, error) {
 	transferTokens := &tokenstrike.TransferTokens{
 		Htlc: secretHex[:],
 		Lock: lockId[:],
