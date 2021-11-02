e5f42a3d7e00fe3f371b13cf30e949f2f01713e0
IntegralTeam <repo@sfxdx.com>
2021-11-02 12:09:50 +0200

    feat: added sending invs and postdata

--- internal/issuer/issuer_token.go
+++ internal/issuer/issuer_token.go
@@ -4,10 +4,15 @@ import (
 	"context"
 	"crypto/sha256"
 	"encoding/hex"
-	"github.com/golang/protobuf/proto"
+	"fmt"
 	"time"
+
 	"token-strike/tsp2p/server/DB"
 	"token-strike/tsp2p/server/rpcservice"
+	"token-strike/tsp2p/server/tokenstrike"
+
+	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc"
 )
 
 func (i *Issuer) IssueToken(ctx context.Context, request *rpcservice.IssueTokenRequest) (*rpcservice.IssueTokenResponse, error) {
@@ -60,7 +65,66 @@ func (i *Issuer) IssueToken(ctx context.Context, request *rpcservice.IssueTokenR
 		return nil, err
 	}
 
+	err = i.tokendb.IssueTokenDB(tokenID, token, block, state)
+	if err != nil {
+		return nil, err
+	}
+
 	return &rpcservice.IssueTokenResponse{
 		TokenId: tokenID,
-	}, i.tokendb.IssueTokenDB(tokenID, token, block, state)
+	}, i.sendBlock(tokenID, blockHash, block)
+}
+
+func (i *Issuer) sendBlock(tokenID string, blockHash [32]byte, block *DB.Block) error {
+	var genError error
+
+	for index, peer := range i.peers {
+		conn, err := grpc.DialContext(
+			context.TODO(),
+			peer,
+			grpc.WithInsecure(),
+		)
+		if err != nil {
+			genError = fmt.Errorf("%v : %s /n %s", index, err, genError)
+		}
+
+		client := rpcservice.NewRPCServiceClient(conn)
+
+		resp, err := client.Inv(
+			context.Background(),
+			&tokenstrike.InvReq{Invs: []*tokenstrike.Inv{
+				{
+					Parent:     blockHash[:],
+					Type:       tokenstrike.TYPE_BLOCK,
+					EntityHash: blockHash[:],
+				},
+			}},
+		)
+		if err != nil {
+			genError = fmt.Errorf("%v : %s /n %s", index, err, genError)
+		}
+
+		if resp.Needed != nil {
+			for _, need := range resp.Needed {
+				if need {
+					DataReq := &tokenstrike.Data{
+						Data: &tokenstrike.Data_Block{Block: block},
+					}
+
+					//send selected lock and NOW skip check of warning
+					_, err := client.PostData(context.TODO(), DataReq)
+					if err != nil {
+						genError = fmt.Errorf("%v : %s /n %s", index, err, genError)
+					}
+
+					err = i.tokendb.ApplyJustification(tokenID, block.Justifications[0])
+					if err != nil {
+						genError = fmt.Errorf("%v : %s /n %s", index, err, genError)
+					}
+				}
+			}
+		}
+	}
+
+	return genError
 }
