845ee450598a35b5168617800f2261b3918fb7ec
IntegralTeam <repo@sfxdx.com>
2021-11-09 10:25:18 +0300

    feat: add id generator

---
+++ internal/utils/IDGen/IDGen.go
@@ -0,0 +1,27 @@
+package idgen
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const separator = ":"
+
+func Encode(blockHash string, number int) string {
+
+	return fmt.Sprint(blockHash, separator, number)
+}
+
+func Decode(ID string) (*string, *int, error) {
+	tuple := strings.Split(ID, separator)
+
+	blockHash := tuple[0]
+
+	justificationNumber, err := strconv.Atoi(tuple[1])
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return &blockHash, &justificationNumber, nil
+}
