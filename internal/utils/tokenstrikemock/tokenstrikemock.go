77a453ae01e4987a2b41c52615582a85916a710f
IntegralTeam <repo@sfxdx.com>
2021-11-09 09:43:11 +0200

    ref: issuer method AddPeer

--- internal/utils/tokenstrikemock/tokenstrikemock.go
+++ internal/utils/tokenstrikemock/tokenstrikemock.go
@@ -15,21 +15,25 @@ const (
 )
 
 type TokenStrikeMock struct {
-	address       address.Address
-	bboltDB       database.DBRepository
-	pktChain      pkt.PktChain
-	addressScheme addressScheme.SimpleAddressScheme
-	invCache      map[string]tokenstrike.Inv
+	address        address.Address
+	bboltDB        database.DBRepository
+	pktChain       pkt.PktChain
+	addressScheme  addressScheme.SimpleAddressScheme
+	invCache       map[string]tokenstrike.Inv
+	mempoolEntries map[string]*MempoolEntry
+	peers          []string
 }
 
 //var _ tokenstrike.TokenStrikeServer = &TokenStrikeMock{}
 
 func New(db database.DBRepository, simpleAddress address.Address) *TokenStrikeMock {
 	return &TokenStrikeMock{
-		bboltDB:       db,
-		pktChain:      &pktchain.SimplePktChain{},
-		addressScheme: addressScheme.SimpleAddressScheme{},
-		invCache:      make(map[string]tokenstrike.Inv),
-		address:       simpleAddress,
+		bboltDB:        db,
+		pktChain:       &pktchain.SimplePktChain{},
+		addressScheme:  addressScheme.SimpleAddressScheme{},
+		invCache:       make(map[string]tokenstrike.Inv),
+		address:        simpleAddress,
+		mempoolEntries: make(map[string]*MempoolEntry, 0),
+		peers:          make([]string, 0),
 	}
 }
