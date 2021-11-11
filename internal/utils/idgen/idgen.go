e3b17af5b94ad990e759ec78cb3db81632c1a388
IntegralTeam <repo@sfxdx.com>
2021-11-11 16:51:00 +0300

    feat: implement EntityIndex

--- internal/utils/idgen/idgen.go
+++ internal/utils/idgen/idgen.go
@@ -1,9 +1,18 @@
 package idgen
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"strconv"
 	"strings"
+	"token-strike/tsp2p/server/DB"
+	"token-strike/tsp2p/server/justifications"
+	"token-strike/tsp2p/server/lock"
+	"token-strike/tsp2p/server/tokenstrike"
+
+	"github.com/golang/protobuf/proto"
 )
 
 const separator = ":"
@@ -23,4 +32,55 @@ func Decode(ID string) (*string, *int, error) {
 	}
 
 	return &blockHash, &justificationNumber, nil
+}
+
+func EntityIndex(Block DB.Block, hashOfEntity []byte) (int, error) {
+	for i, justification := range Block.Justifications {
+		if isEntity(justification, hashOfEntity) {
+			return i, nil
+		}
+	}
+
+	return 0, fmt.Errorf("Justification with %v content hash, not found", hashOfEntity)
+}
+
+func isEntity(justification *DB.Justification, hashOfEntity []byte) bool {
+	switch data := justification.Content.(type) {
+	case *DB.Justification_Lock:
+		return isLockEqual(data.Lock.Lock, hashOfEntity)
+	case *DB.Justification_Transfer:
+		return isTransferEqual(data.Transfer, hashOfEntity)
+	}
+
+	return false
+}
+
+func isLockEqual(lock *lock.Lock, entity []byte) bool {
+	hash, err := lock.GetHash()
+	if err != nil {
+		return false
+	}
+
+	return bytes.Equal(hash, entity)
+}
+
+func isTransferEqual(justificationTX *justifications.TranferToken, entity []byte) bool {
+	htlc, err := hex.DecodeString(justificationTX.HtlcSecret)
+	if err != nil {
+		return false
+	}
+
+	tx := &tokenstrike.TransferTokens{
+		Htlc:   htlc,
+		LockId: justificationTX.Lock,
+	}
+
+	txBytes, err := proto.Marshal(tx)
+	if err != nil {
+		return false
+	}
+
+	hash := sha256.Sum256(txBytes)
+
+	return bytes.Equal(hash[:], entity)
 }
